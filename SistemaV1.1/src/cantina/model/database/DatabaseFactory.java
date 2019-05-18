1f580d3e440b1dd2d9b9f61a3bcd7b01da0fb722
Artur Paranayba <artur.paranayba@gmail.com>
2019-05-18 12:17:17 -0300

    Alteração na lógica do codigo

--- SistemaV1.1/src/cantina/model/database/DatabaseFactory.java
+++ SistemaV1.1/src/cantina/model/database/DatabaseFactory.java
@@ -4,9 +4,7 @@ public class DatabaseFactory {
     public static Database getDatabase(String nome){
         if(nome.equals("postgresql")){
             return new DatabasePostgreSQL();
-        }else if(nome.equals("mysql")){
-            return new DatabaseMySQL();
         }
         return null;
-    }
+    }       
 }
